e163fc363707c2300f280a99b2295236313c0b1b
neerooayusuf <40438675+neerooayusuf@users.noreply.github.com>
2026-06-01 20:55:32 +0400

    Include icon files in service worker cache
    Added icon files to the service worker shell cache.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'household-v1';
-const SHELL = ['/', '/index.html'];
+const SHELL = ['/', '/index.html','./icons/icon-192.png','./icons/icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
